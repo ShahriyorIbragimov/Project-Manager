cd6569b2076488e4ea90f34540fc7b4af34a08ee
Shahriyor Ibragimov <magmainyourvein@gmail.com>
2025-11-15 15:47:18 +0500

    commit

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,27 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { ThemeProvider } from "@/components/theme-provider"
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
 
 const RootLayout = () => (
   <>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Outlet />
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
     </ThemeProvider>
   </>
 )
